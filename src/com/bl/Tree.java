946e687ebe28930b0c8904635ea07a19b4561c2f
dipalimagare <kalpeshmahajan90@gmail.com>
2021-08-15 23:51:22 +0530

    Uploading Binary Search tree,Hashmap

---
+++ src/com/bl/Tree.java
@@ -0,0 +1,18 @@
+package com.bl;
+
+interface Tree<T extends Comparable<T>> {
+	void add(T data);
+
+	void preOrder();
+
+	void postOrder();
+
+	void inOrder();
+
+	void bfs();
+
+	public boolean isEmpty();
+
+	public boolean search(T data);
+
+}
